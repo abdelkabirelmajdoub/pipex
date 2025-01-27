1bf5d3a499caaf8927d1f1028f1c9b1ae342d03d
SMART <56655744+abdelkabirelmajdoub@users.noreply.github.com>
2025-01-27 16:23:50 +0100

    Update ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -16,13 +16,11 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char			*d;
 	const unsigned char		*s;
-	size_t					i;
 
 	if (dest == src)
 		return (dest);
 	d = dest;
 	s = src;
-	i = 0;
 	if (d < s)
 		ft_memcpy(dest, src, n);
 	else
